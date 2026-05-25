4612a9a9a453c25b0de539dfd2f056d30e22356a
jordanalaicrisosto-cyber <jordanalaicrisosto@gmail.com>
2026-05-25 04:21:38 +0200

    21 à finir

--- Incomplet/21/Solution.py
+++ Incomplet/21/Solution.py
@@ -7,23 +7,52 @@ class Solution:
             return list2
         if list2 == None :
             return list1
-        
-        nodes = ListNode()
+
         len1 = 0
         len2 = 0
         pList1 = list1
         pList2 = list2
 
+
         while pList1.next != None :
-            len += 1
+            len1 += 1
             assert 0 <= pList1.val <= 50
             assert pList1.val <= pList1.next.val
-            pList1 = list1.next
+            pList1 = pList1.next
         while pList2.next != None :
-            len += 1
+            len2 += 1
             assert 0 <= pList2.val <= 50
             assert pList2.val <= pList2.next.val
-            pList2 = list2.next
-        assert len <= 50
-        
-        return nodes
+            pList2 = pList2.next
+        assert len1 <= 50 and len2 <= 50
+
+        pList1 = list1
+        pList2 = list2
+        merge = ListNode(min(pList1.val, pList2.val))
+        if merge.val == pList1.val :
+            pList1 = pList1.next
+        else :
+            pList2 = pList2.next
+
+        while pList1.next != None and pList2.next != None :
+            merge.next = ListNode(min(pList1.val, pList2.val))
+            if merge.val == pList1.val :
+                pList1 = pList1.next
+            else :
+                pList2 = pList2.next
+
+        if pList1 != None :
+            while pList1.next != None :
+                merge.next = pList1
+                pList1 = pList1.next
+        else :
+            while pList2 != None :
+                merge.next = pList2
+                pList2 = pList2.next
+
+        pMerge = merge
+        while pMerge.next != None :
+            print(pMerge.val)
+            pMerge = pMerge.next
+
+        return merge
--- Incomplet/21/TestCase.py
+++ Incomplet/21/TestCase.py
@@ -1,12 +1,13 @@
 import Solution
+from ListNode import ListNode
 
 test = Solution.Solution()
 
 #Case 1
-print(test.mergeTwoLists([1,2,4], [1,3,4])) #[1,1,2,3,4,4]
+print(test.mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4))))) #[1,1,2,3,4,4]
 
 #Case 2
-print(test.mergeTwoLists([], [])) #[]
+print(test.mergeTwoLists(None, None)) #[]
 
 #Case 3
-print(test.mergeTwoLists([], [0])) #[0]
+print(test.mergeTwoLists(None, ListNode())) #[0]
