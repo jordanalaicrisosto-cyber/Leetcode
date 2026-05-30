803c8a37f188b5e639a3f082a190499eebba7194
jordanalaicrisosto-cyber <jordanalaicrisosto@gmail.com>
2026-05-30 20:06:44 +0200

    J'y arrive pas le 14

---
+++ Incomplet/14/TestCase.py
@@ -0,0 +1,21 @@
+import Solution
+
+test = Solution.Solution()
+
+#Case 1
+print(test.longestCommonPrefix(["flower","flow","flight"])) #fl
+
+#Case 2
+print(test.longestCommonPrefix(["dog","racecar","car"])) #
+
+#Case 3
+print(test.longestCommonPrefix(["cir","car"])) #c
+
+#Case 4
+print(test.longestCommonPrefix(["a"])) #a
+
+#Case 5
+print(test.longestCommonPrefix(["ab", "a"])) #a
+
+#Case 6
+print(test.longestCommonPrefix(["reflower","flow","flight"])) #
